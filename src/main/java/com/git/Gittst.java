e5011a17e613a18e8c7ba7e520a5a263121d6e6b
ka <ka@Kya.com>
2021-05-05 10:23:14 +0800

    second commit

--- src/main/java/com/git/Gittst.java
+++ src/main/java/com/git/Gittst.java
@@ -2,6 +2,6 @@ package com.git;
 
 public class Gittst {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello githard");
     }
 }
