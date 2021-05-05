b91f6c2656a8b6f8c316bc97aff9e4d358ca3307
ka <ka@Kya.com>
2021-05-05 12:07:05 +0800

    push

--- src/main/java/com/git/Gittst.java
+++ src/main/java/com/git/Gittst.java
@@ -2,9 +2,6 @@ package com.git;
 
 public class Gittst {
     public static void main(String[] args) {
-        System.out.println("hello githard333");
-        System.out.println("hello githard555");
-        System.out.println("master");
-        System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
