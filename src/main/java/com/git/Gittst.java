eed2ccc24bafaed7a6c9c313a1da3a87870b0f0e
ka <ka@Kya.com>
2021-05-05 10:38:12 +0800

    hot-fix commit

--- src/main/java/com/git/Gittst.java
+++ src/main/java/com/git/Gittst.java
@@ -3,5 +3,6 @@ package com.git;
 public class Gittst {
     public static void main(String[] args) {
         System.out.println("hello githard333");
+        System.out.println("hello githard555");
     }
 }
